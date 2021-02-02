3b4dff7d4e1f367b916ac090e24c7788ecb9db07
Vishnevskiy Vlaimir Home Laptop <vishnevskiy.vladimir.mbox@gmail.com>
2021-02-02 13:05:20 +0300

    change SDK to OpenJDK15. add Statement with DDL operations

--- src/com/vishdev/jdbc/starter/JdbcRunner.java
+++ src/com/vishdev/jdbc/starter/JdbcRunner.java
@@ -5,12 +5,22 @@ import org.postgresql.Driver;
 
 
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class JdbcRunner {
     public static void main(String[] args) {
         Class<Driver> driverClass = Driver.class;
-        try (var connection = ConnectionManager.open()) {
+        //CREATE SCHEMA my;
+        String sql = """
+                  
+                CREATE TABLE book (
+                id SERIAL PRIMARY KEY
+               )   ;         
+                """;
+        try (var connection = ConnectionManager.open();
+             var statement = connection.createStatement()) {
             System.out.println(connection.getTransactionIsolation());
+            statement.execute(sql);
         } catch (SQLException e) {
             e.printStackTrace();
         }
