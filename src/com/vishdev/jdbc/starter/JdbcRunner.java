824d58b0b18ceb4f60e50185f0d2dfdadca2d669
Vladimir Vishnevskiy <vishnevskiy.vladimir.mbox@gmail.com>
2021-03-09 18:40:37 +0300

    Add example of DML (UPDATE, INSERT, DELETE)
    For those operations its better to use statement.executeUpdate(), because it returns number of modified items.
    
    We cannot use .executeUpdate() if our SQL query contains RETURNING. In that case we can use execute wich returnes boolean (true - select and operations with returning, false - others)

--- src/com/vishdev/jdbc/starter/JdbcRunner.java
+++ src/com/vishdev/jdbc/starter/JdbcRunner.java
@@ -8,17 +8,26 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class JdbcRunner {
+
+    public static final String INSERT_EXAMPLE = """
+               INSERT INTO info (data)
+               VALUES
+               ('TEST1'),
+               ('TEST2'),
+               ('TEST3'),
+               ('TEST1');
+            """;
+    public static final String UPDATE_EXAMPLE = """
+               UPDATE info 
+               SET data = 'testTest'
+               WHERE id = 3;
+            """;
+
     public static void main(String[] args) {
         Class<Driver> driverClass = Driver.class;
-        String sql = """
-                CREATE TABLE IF NOT EXISTS info (
-                id SERIAL PRIMARY KEY ,
-                data TEXT NOT NULL 
-                )     
-                """;
         try (var connection = ConnectionManager.open();
              var statement = connection.createStatement()) {
-            boolean executeResult = statement.execute(sql);
+            int executeResult = statement.executeUpdate(UPDATE_EXAMPLE);
             System.out.println(executeResult);
         } catch (SQLException e) {
             e.printStackTrace();
