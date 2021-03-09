6fdabe8ba60bf8ead221e4c992a4530e3899958f
Vladimir Vishnevskiy <vishnevskiy.vladimir.mbox@gmail.com>
2021-03-09 18:02:41 +0300

    Add example of DML operation
    For DML operations  statement.execute() is commonly used.

--- src/com/vishdev/jdbc/starter/JdbcRunner.java
+++ src/com/vishdev/jdbc/starter/JdbcRunner.java
@@ -10,17 +10,16 @@ import java.sql.Statement;
 public class JdbcRunner {
     public static void main(String[] args) {
         Class<Driver> driverClass = Driver.class;
-        //CREATE SCHEMA my;
         String sql = """
-                  
-                CREATE TABLE book (
-                id SERIAL PRIMARY KEY
-               )   ;         
+                CREATE TABLE IF NOT EXISTS info (
+                id SERIAL PRIMARY KEY ,
+                data TEXT NOT NULL 
+                )     
                 """;
         try (var connection = ConnectionManager.open();
              var statement = connection.createStatement()) {
-            System.out.println(connection.getTransactionIsolation());
-            statement.execute(sql);
+            boolean executeResult = statement.execute(sql);
+            System.out.println(executeResult);
         } catch (SQLException e) {
             e.printStackTrace();
         }
