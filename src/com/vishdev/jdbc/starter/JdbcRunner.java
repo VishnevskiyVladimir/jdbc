64522c9657adef92e08b9824c539e5ad867337ed
Vladimir Vishnevskiy <vishnevskiy.vladimir.mbox@gmail.com>
2021-03-15 01:23:05 +0300

    Add example of prepareStatement
    All methods of prepareStatement don't take sql as parameter. Sql is transfer to static factory.
    
    In SQL for preparedStatement all unknown parameters should be replaced by "?".
    
     We set them before executeQuery();

--- src/com/vishdev/jdbc/starter/JdbcRunner.java
+++ src/com/vishdev/jdbc/starter/JdbcRunner.java
@@ -5,36 +5,62 @@ import org.intellij.lang.annotations.Language;
 import org.postgresql.Driver;
 
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+
+import static java.time.LocalTime.now;
 
 public class JdbcRunner {
 
     public static void main(String[] args) throws SQLException {
-        String flightId = "2"; // CORRECT CODE
-//        String flightId = "2 OR 1 = 1"; // SQL INJECTION RETURNING ALL TICKETS
-//        String flightId = "2 OR 1 = 1; DROP TABLE info;"; // SQL INJECTION DROPPING TABLE
-        System.out.println(getTicketIdsByFlightId(flightId));
+        Long flightId = 2L;
+//        System.out.println(getTicketsByFlightId(flightId));
+        System.out.println(getFlightsBetween(LocalDate.of(2020,10,1).atStartOfDay(), LocalDateTime.now()));
 
     }
 
-    public static List<Long> getTicketIdsByFlightId(String flightId) throws SQLException {
+    private static List<Long> getFlightsBetween(LocalDateTime start, LocalDateTime end) throws SQLException {
+        @Language(Queries.SQL_DIALECT)
+        String sql = """
+                SELECT id
+                FROM flight
+                WHERE departure_date BETWEEN ? AND ?
+                """;
+        ArrayList<Long> flights = new ArrayList<>();
+
+        try (Connection connection = ConnectionManager.open();
+             var preparedStatement = connection.prepareStatement(sql)) {
+            System.out.println(preparedStatement);
+            preparedStatement.setTimestamp(1, Timestamp.valueOf(start));
+            System.out.println(preparedStatement);
+            preparedStatement.setTimestamp(2, Timestamp.valueOf(end));
+            System.out.println(preparedStatement);
+            var resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                //tickets.add(resultSet.getLong("id"));
+                flights.add(resultSet.getObject("id", Long.class)); //NULL SAFE
+            }
+            return flights;
+        }
+    }
+
+    private static List<Long> getTicketsByFlightId(Long flightId) throws SQLException {
         @Language(Queries.SQL_DIALECT)
         String sql = """
                 SELECT id
                 FROM ticket
-                WHERE flight_id = %s
-                """.formatted(flightId);
+                WHERE flight_id = ?
+                """;
 
         ArrayList<Long> tickets = new ArrayList<>();
         try (Connection connection = ConnectionManager.open();
-        Statement statement = connection.createStatement()) {
-            var resultSet = statement.executeQuery(sql);
-            while (resultSet.next()){
+             var preparedStatement = connection.prepareStatement(sql)) {
+            preparedStatement.setLong(1, flightId);
+            var resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
                 //tickets.add(resultSet.getLong("id"));
                 tickets.add(resultSet.getObject("id", Long.class)); //NULL SAFE
             }
